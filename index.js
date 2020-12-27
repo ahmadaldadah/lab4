45d5317c9f6812a1a02ed52b13f0f87b1ea7f829
ahmadaldadah <34897132+ahmadaldadah@users.noreply.github.com>
2020-12-27 22:19:06 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const router = express.Router();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+router.post('/data',(request,response) => {
+
+    response.send({"message":"Hello","name":"Ahmad"})
+});
+router.get('/greeting/:name',(request,response) => {
+    var d = req.param.name;
+    
+    response.send({"message":"Hello","name":d})
+});
+app.use( router);
+app.listen(3000)
